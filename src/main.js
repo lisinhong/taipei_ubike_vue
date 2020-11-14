92da7cf337222dd8a4343780ec4bd33e6c320870
alanli <alan12141020@gmail.com>
2020-11-14 20:21:04 +0800

    Setup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import App from './App.vue';
 import store from './store';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'leaflet/dist/leaflet.css';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionAPI);
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 new Vue({
   store,
