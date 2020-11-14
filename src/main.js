1bcc2041301f7b1248ca92c99ccffeb88150c0af
alanli <alan12141020@gmail.com>
2020-11-14 20:45:04 +0800

    Fix leaflet marker icon missing

--- src/main.js
+++ src/main.js
@@ -1,11 +1,21 @@
+/* eslint-disable global-require */
 import Vue from 'vue';
 import VueCompositionAPI from '@vue/composition-api';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import { Icon } from 'leaflet';
 import App from './App.vue';
 import store from './store';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import 'leaflet/dist/leaflet.css';
+
+// eslint-disable-next-line no-underscore-dangle
+delete Icon.Default.prototype._getIconUrl;
+Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 Vue.config.productionTip = false;
 
